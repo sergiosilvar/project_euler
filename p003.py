f17c8883464470debfcd1f0c9ed70abf9de9a9e6
Sérgio Rodrigues <srodriguex@gmail.com>
2015-08-05 21:37:54 -0300

    Better solution for problem 003.

--- p003.py
+++ p003.py
@@ -1,21 +1,37 @@
 
 # What is the largest prime factor of the number 600851475143 ?
-from numpy import array
-from math import sqrt
-
 
 N = 600851475143
-# https://primes.utm.edu/lists/small/100000.txt
-with open('assets/100000.txt', 'r') as f:
-    text = f.read()
-x = 'see http://primes.utm.edu/\n\n\n\n   '
-text = text[text.find(x)+len(x):]
-text = text.replace('\n\n', '')
-primes = array([int(i.strip()) for i in text.split(' ') if i.strip() != ''])
-primes = primes[primes < sqrt(N)]
-    
-primes = primes[::-1]
-for i in primes:
+
+def primes(n):
+    '''
+    Prime numbers up to n generator.
+    Source: http://programmingpraxis.com/programming-with-prime-numbers-source-code-in-python/
+    '''
+    if type(n) != int and type(n) != long:
+        raise TypeError('must be integer')
+    if n < 2:
+        raise ValueError('must be greater than one')
+    m = (n-1) // 2
+    b = [True] * m
+    i, p, ps = 0, 3, [2]
+    while p*p < n:
+        if b[i]:
+            ps.append(p)
+            j = 2*i*i + 6*i + 3
+            while j < m:
+                b[j] = False
+                j = j + 2*i + 3
+        i += 1; p += 2
+    while i < m:
+        if b[i]:
+            ps.append(p)
+        i += 1; p += 2
+    return ps
+
+
+myprimes = primes(int(sqrt(N)))
+
+for i in myprimes:
     if N%i == 0:
         print(i)
-        break
